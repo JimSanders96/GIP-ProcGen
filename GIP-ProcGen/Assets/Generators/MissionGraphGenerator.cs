ef166483bc1ff0eae6e3a63cc4cf16bda609aa62
Jimbolul <jimsandersgamedev@gmail.com>
2017-10-11 18:06:05 +0200

    Added some classes to begin generating missions

---
+++ GIP-ProcGen/Assets/Generators/MissionGraphGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionGraphGenerator : MonoBehaviour
+{
+    [Tooltip("The amount of challenges in the mission")]
+    public int missionLength = 3;
+
+    [Tooltip("These mechanics are used to create challenges")]
+    public Mechanics[] requiredMechanics;
+
+    public void GenerateMission()
+    {
+
+    }
+}
---
+++ GIP-ProcGen/Assets/GraphGrammar/Mechanics.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Mechanics
+{
+    MECHANIC_1,
+    MECHANIC_2,
+    MECHANIC_3
+}
